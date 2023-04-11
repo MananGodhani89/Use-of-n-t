ef98d0d6587e86fd149c24f89c5277a05595209b
MananGodhani89 <130462891+MananGodhani89@users.noreply.github.com>
2023-04-11 17:13:07 +0530

    Add files via upload

---
+++ 3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+
+main()
+{
+	printf("- - - - - \n");
+	printf("|\t | \n");
+	printf("M\t |\n");
+	printf("A\t |\n");
+	printf("N\t |\n");
+	printf("A\t |\n");
+	printf("N\t |\n");
+	printf("|\t | \n");
+	printf("- - - - - \n");
+}
